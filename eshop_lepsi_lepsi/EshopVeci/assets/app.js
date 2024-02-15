101f5302a2517b54e09146a7ac6780dee155b676
Erik Vaněk <erikvanek0@gmail.com>
2024-02-15 18:36:12 +0100

    Header PC/Mobile done

--- eshop_lepsi_lepsi/EshopVeci/assets/app.js
+++ eshop_lepsi_lepsi/EshopVeci/assets/app.js
@@ -10,4 +10,20 @@ import './styles/header.css';
 import './styles/main.css';
 
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+//console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Burger menu controller
+let categories = document.getElementsByClassName("header-categories")[0];
+document.getElementById("brgr").addEventListener("click", function(event) {
+    event.preventDefault(); //Prevent refresh
+    if (categories.style.display === "none") categories.style.display = "block";
+    else categories.style.display = "none";
+});
+
+document.getElementById("brgr").click();
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 760) categories.style.display = "block";
+    else categories.style.display = "none";
+});
+
